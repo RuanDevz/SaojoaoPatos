b6ce34971c86dc8e70a3bea89d6df03c8e5f3510
RuanDevz <Ruanbatista1509@outlook.com>
2024-02-06 19:31:38 -0300

    intial commit

--- saojoao/src/Questions/Start.jsx
+++ saojoao/src/Questions/Start.jsx
@@ -8,10 +8,10 @@ const Start = () => {
 
 
   
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
   return (
-    <div>
+    <div className='divgeral'>
       <header className='container_logo'>
         <img src={logo} alt="Logo" />
       </header>
@@ -20,13 +20,14 @@ const Start = () => {
         <p>O São João sem dúvidas foi um sucesso! Mas como sempre queremos deixar tudo cada vez melhor. Por isso, precisamos saber o que você achou da nossa festa. Então bora contar tudo na pesquisa de satisfação?</p>
 
        <p><span id='ocean'>Queremos saber:</span> O que curtiu, o que não curtiu e como podemos fazer o São João da gente ficar ainda mais nossa cara nas próximas edições.</p>
+
        <p className='paragrafo-styled'><span id='orange'>Contamos </span><span>com </span><span id='pink'>seu <br /> Feedback </span><span>vamos lá? </span></p>
         </section>
       </main>
       <section className='start-segund-section'>
         <form action="" method='POST' className='container_input'>
           <input type="text" placeholder='Digite seu e-mail...' />
-          <button><p className='name_button'>INICIAR</p></button>
+          <button onClick={() => navigate('/question1')}><p className='name_button'>INICIAR</p></button>
         </form>
         </section>
     </div>

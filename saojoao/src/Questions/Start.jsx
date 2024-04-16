4e916433bddaabc1e18aa27b1f59557827958750
RuanDevz <Ruanbatista1509@outlook.com>
2024-04-16 19:08:42 -0300

    verificação de telefone

--- saojoao/src/Questions/Start.jsx
+++ saojoao/src/Questions/Start.jsx
@@ -18,35 +18,38 @@ const Start = () => {
     if (!validateNumber(number)) {
       setError("Número inválido");
     } else {
-      axios.post('http://localhost:3000/tel',{
+      axios.post('http://localhost:3000/tel', {
         Telefone: number
       })
         .then((response) => {
-          console.log(response.data);
+          if (response.data.error) {
+            console.log(response.data.error);
+            setError(response.data.error);
+          } else {
+            const currentDate = new Date();
+            const day = String(currentDate.getDate()).padStart(2, '0');
+            const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+            const year = currentDate.getFullYear();
+            const formattedDate = `${day}/${month}/${year}`;
+            const hours = String(currentDate.getHours()).padStart(2, '0');
+            const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+            const formattedTime = `${hours}:${minutes}`;
+            const dateTimeString = `${formattedDate} ${formattedTime}`;
+            const numberString = `${number}`;
+
+            _feedbacks.unshift(numberString);
+            _feedbacks.unshift(dateTimeString);
+            setFeedbacks(_feedbacks);
+            console.log(_feedbacks);
+            navigate('/question2');
+          }
         })
         .catch((error) => {
-          console.error('Erro na requisição:', error);
-          setError('Ocorreu um erro ao enviar a requisição.');
+          console.error('Erro ao enviar o número:', error);
+          setError('Erro ao enviar o número, por favor, tente novamente.');
         });
-
-      const currentDate = new Date();
-      const day = String(currentDate.getDate()).padStart(2, '0');
-      const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-      const year = currentDate.getFullYear();
-      const formattedDate = `${day}/${month}/${year}`;
-      const hours = String(currentDate.getHours()).padStart(2, '0');
-      const minutes = String(currentDate.getMinutes()).padStart(2, '0');
-      const formattedTime = `${hours}:${minutes}`;
-      const dateTimeString = `${formattedDate} ${formattedTime}`;
-      const numberString = `${number}`;
-
-      _feedbacks.unshift(numberString);
-      _feedbacks.unshift(dateTimeString);
-      setFeedbacks(_feedbacks);
-      console.log(_feedbacks);
-      navigate('/question2');
     }
-  }
+  };
 
   const validateNumber = (number) => {
     const re = /^\(\d{2}\) \d{5}-\d{4}$/;
@@ -92,6 +95,6 @@ const Start = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Start;
